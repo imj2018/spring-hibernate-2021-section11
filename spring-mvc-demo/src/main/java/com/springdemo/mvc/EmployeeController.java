e3480488277659833212c44e650999c056a00c9d
imj <imj.programming.2013@gmail.com>
2021-06-30 13:38:48 +1000

    129. Text Fields - Write Some Code - Part 2

--- spring-mvc-demo/src/main/java/com/springdemo/mvc/EmployeeController.java
+++ spring-mvc-demo/src/main/java/com/springdemo/mvc/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,7 +21,19 @@ public class EmployeeController {
 		// name of the attribute is "employee", value of the attribute is employee created line 15
 		model.addAttribute("employee", employee);
 		
-		
 		return "employee-form";
+	}
+	
+	
+	@RequestMapping("/processForm")
+	public String processForm(
+			// bind to the model attribute passed in
+			// the form data will populate the employee object to be accessible, spring handles
+			// this instead of HttpServletRequest request/request.getParameter 
+			@ModelAttribute("employee") Employee employee) {
+		
+		System.out.println("employee: " + employee.getFirstName() + " " + employee.getLastName());
+		
+		return "employee-confirmation";
 	}
 }
---
+++ spring-mvc-demo/src/main/webapp/WEB-INF/view/employee-form.jsp
@@ -0,0 +1,38 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title> Employee Contract Form </title>
+</head>
+
+<body>
+ 
+ 	<%-- spring mvc form tags form:form, form:input --%>
+ 	<%-- model attribute must match the atrribute name given in controller "employee" i.e 
+ 	model.addAttribute("employee", employee); --%> 
+ 	<form:form action="processForm" modelAttribute="employee">
+ 	
+ 	<%-- property of Employee class, spring mvc will call employee.getFirstName 
+ 	behind the scences --%>
+ 	First name: <form:input path="firstName" />
+ 	
+ 	<br><br>
+ 	
+ 	First name: <form:input path="firstName" />
+ 	
+ 	<br><br>
+ 	
+ 	<%-- when submitted the setters are called student.setFirstName for whatever
+ 	 is passed in the fields by the user --%>
+ 	<input type="submit" value="Submit" />
+ 	
+ 	</form:form>
+ 	
+ 	
+</body>
+
+</html>
+
