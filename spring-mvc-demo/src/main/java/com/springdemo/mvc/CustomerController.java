eb8d742f6e4ccccc1ea377140e4746e057de9c3d
imj <imj.programming.2013@gmail.com>
2021-07-01 18:25:53 +1000

    149. Perform Validation in Controller Class - Part 2

--- spring-mvc-demo/src/main/java/com/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/main/java/com/springdemo/mvc/CustomerController.java
@@ -1,7 +1,13 @@
 package com.springdemo.mvc;
 
+
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +20,22 @@ public class CustomerController {
 		model.addAttribute("customer", new Customer());
 		
 		return "customer-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(
+			// @Valid will perform validation rules on Customer object
+			// "Spring will store results of validation in BindingResult object"
+			@Valid @ModelAttribute("customer") Customer customer,
+			BindingResult bindingResult) {
+		
+		if (bindingResult.hasErrors()) {
+			return "customer-form";
+		}
+		else {
+			return "customer-confirmation";
+		}
+		
 	}
 	
 }
