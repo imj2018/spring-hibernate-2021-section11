f415301c0bab8fc8a8668b1a0dcf0a43aef67e95
imj <imj.programming.2013@gmail.com>
2021-07-01 18:57:15 +1000

    153. Add Pre-processing Code with @InitBinder - Write Some Code

--- spring-mvc-demo/src/main/java/com/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/main/java/com/springdemo/mvc/CustomerController.java
@@ -4,15 +4,31 @@ package com.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	// add an init binder .. to convert trim input strings
+	// remove leading and trailing whitespace
+	// resolve issues of our validation
+	// @InitBinder pre-processes and will be called for web requests coming to the controller
+	@InitBinder
+	public void InitBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
